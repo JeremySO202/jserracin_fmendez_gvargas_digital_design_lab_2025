27b5bccb920f6a0ec22ae96c6151c13d418e250c
f4bms <fmelendezsequeira@gmail.com>
2025-03-18 13:33:31 -0600

    alu con restador
    alu listo con restador y demás operaciones, haría falta la division y el modulo

--- laboratorio_2/sumador_n/alu.sv
+++ laboratorio_2/sumador_n/alu.sv
@@ -24,24 +24,40 @@ module alu #(
 		.y(sum_y),
 		.cout(sum_cout)
 	);
+	
+	//variables para la suma
+	logic [W-1:0] subs_y;
+	logic subs_cout;
+	
+	nBitsSubstractor #(.n(W)) subs (
+		.a(A),
+		.b(B),
+		.s(subs_y),
+		.cout(subs_cout)
+	);
+	
 
 always_comb begin
+
 	flags = 4'b0000;
+
 	case(op)
 	//pongo la asignación del flag ahí solo pq es la suma, si lo pongo afuera se asigna siempre pero no sé como se use sin la suma
 		4'b0000: begin
 		op_result = sum_y;
 		flags[0] = sum_cout;
 		end
-		4'b0001: op_result = A - B; // cambiar la resta
+		4'b0001: op_result = subs_y; // agregarle las flags a la resta
 		4'b0010: op_result = A * B;
 		4'b0011: op_result = A / B;
 		4'b0100: op_result = A % B;
 		4'b0101: op_result = A & B;
 		4'b0110: op_result = A | B;
 		4'b0111: op_result = A ^ B;
-		/* falta el shift left y shift right entonces aquí se debe agregar*/
+		4'b1000: op_result = A << 1;
+		4'b1001: op_result = A >> 1;
+		default: op_result = 0;
 		endcase
 	end
 		
-end module
+endmodule
