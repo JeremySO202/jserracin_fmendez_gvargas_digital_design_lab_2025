7a8454e3c08048251b74d95f71ad142a9e051e80
Gabriel Vargas López <gavalo.99@gmail.com>
2025-04-21 12:29:08 -0600

    Creación de archivo de enums para nombrar estados en la fsm

---
+++ laboratorio_3/FSM/connect4_tipos.sv
@@ -0,0 +1,26 @@
+// Añadir guardas de inclusión para prevenir múltiples declaraciones
+`ifndef CONNECT4_TYPES_SV
+`define CONNECT4_TYPES_SV
+
+package connect4_tipos;
+
+
+typedef enum logic [4:0] {
+    INICIO,           // Estado inicial
+	 MENU,
+	 JUGADOR_INICIAL,
+    TURNO_J1,   // Turno del jugador 1 (FPGA)
+	 FIN_TURNO_J1,
+	 RANDOM_J1,
+	 VERIFICAR_J1,
+	 GANADOR_J1,
+    TURNO_J2,   // Turno del jugador 2 (Arduino)
+	 VERIFICAR_J2,
+	 RANDOM_J2,
+	 GANADOR_J2,
+	 FIN_JUEGO
+} game_state;
+
+endpackage
+
+`endif // CONNECT4_TYPES_SV
